704110544cdd262550ec126e59e40cbf4f51bd4a
thederpykrafter <thederpykrafter@gmail.com>
2024-05-29 16:24:21 -0400

    oil

--- lua/thederpykrafter/plugins/oil.lua
+++ lua/thederpykrafter/plugins/oil.lua
@@ -2,6 +2,9 @@ return {
   'stevearc/oil.nvim',
   dependencies = { "nvim-tree/nvim-web-devicons"},
   opts = {
-    view_options = { show_hidden = true }
+    keymaps = {
+      ["q"] = "actions.close",
+      ["H"] = "actions.toggle_hidden",
+    },
   }
 }
