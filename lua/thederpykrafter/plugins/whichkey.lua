1bf48d31c8ec481b97f0df0391b7ac9d437f4a9f
thederpykrafter <thederpykrafter@gmail.com>
2024-08-04 09:51:35 -0400

    git whichkey

--- lua/thederpykrafter/plugins/whichkey.lua
+++ lua/thederpykrafter/plugins/whichkey.lua
@@ -5,6 +5,7 @@ return {
     require("which-key").setup()
     require("which-key").register {
       -- Lazy Prefix
+      ["<leader>g"] = { name = "Git", _ = "which_key_ignore"},
       ["<leader>l"] = { name = "Lazy", _ = "which_key_ignore"},
       ["<leader>s"] = { name = "Search", _ = "which_key_ignore"},
       ["<leader>d"] = { name = "Diagnostic", _ = "which_key_ignore"}
