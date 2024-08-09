ceee58ee0854589fcecd019e9628c3ceb0ada8bb
thederpykrafter <thederpykrafter@gmail.com>
2024-08-08 20:29:47 -0400

    whichkey

--- lua/thederpykrafter/plugins/whichkey.lua
+++ lua/thederpykrafter/plugins/whichkey.lua
@@ -1,19 +1,18 @@
 return {
-  "folke/which-key.nvim",
-  event = "VimEnter",
-  config = function()
-    require("which-key").setup()
-    require("which-key").add({
-      -- Lazy Prefix
-      { "<leader>d", group = "Diagnostic" },
-      { "<leader>d_", hidden = true },
-      { "<leader>g", group = "Git" },
-      { "<leader>g_", hidden = true },
-      { "<leader>l", group = "Lazy" },
-      { "<leader>l_", hidden = true },
-      { "<leader>s", group = "Search" },
-      { "<leader>s_", hidden = true },
-    })
-  end
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	config = function()
+		require("which-key").setup()
+		require("which-key").add({
+			-- Lazy Prefix
+			{ "<leader>d", group = "Diagnostic" },
+			{ "<leader>d_", hidden = true },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>g_", hidden = true },
+			{ "<leader>l", group = "Lazy" },
+			{ "<leader>l_", hidden = true },
+			{ "<leader>s", group = "Search" },
+			{ "<leader>s_", hidden = true },
+		})
+	end,
 }
-
