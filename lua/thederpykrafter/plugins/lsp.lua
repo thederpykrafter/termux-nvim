9666e0115e36e5d2a3813630d2745f98bfc2c70f
thederpykrafter <thederpykrafter@gmail.com>
2024-08-16 12:53:57 -0400

    fidget

--- lua/thederpykrafter/plugins/lsp.lua
+++ lua/thederpykrafter/plugins/lsp.lua
@@ -3,7 +3,8 @@ return {
   {'hrsh7th/cmp-nvim-lsp'},
   {'hrsh7th/nvim-cmp'},
   {'L3MON4D3/LuaSnip'},
-  {'folke/neodev.nvim', opts = {}}
+  {'folke/neodev.nvim', opts = {}},
+  {'j-hui/fidget.nvim', opts = {}}
 }
 
 --require('nvim-lspconfig').lua_ls.setup{}
