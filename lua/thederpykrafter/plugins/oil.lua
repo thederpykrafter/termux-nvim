a54658b9f91ebb3c5b906092196f201c7b409012
thederpykrafter <thederpykrafter@gmail.com>
2024-08-16 15:24:55 -0400

    update oil keymaps

--- lua/thederpykrafter/plugins/oil.lua
+++ lua/thederpykrafter/plugins/oil.lua
@@ -1,10 +1,12 @@
 return {
-  'stevearc/oil.nvim',
-  dependencies = { "nvim-tree/nvim-web-devicons"},
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     keymaps = {
+      ["?"] = "actions.show_help",
       ["q"] = "actions.close",
       ["H"] = "actions.toggle_hidden",
     },
-  }
+    watch_for_changes = true,
+  },
 }
