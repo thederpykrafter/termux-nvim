e165e140e7f1414d9495af516940f59561213fda
thederpykrafter <thederpykrafter@gmail.com>
2024-08-18 23:58:35 -0400

    new-plugin for comp-run from scratch

--- lua/thederpykrafter/plugins/comp-run.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "thederpykrafter/comp-run.nvim",
-  dir = "~/Dev/lua/comp-run.nvim",
-  opts = {}
-}
---
+++ lua/thederpykrafter/plugins/new-plugin.lua
@@ -0,0 +1,10 @@
+return {
+	"thederpykrafter/new-plugin.nvim",
+	dir = "~/Dev/lua/new-plugin.nvim",
+	opts = {
+		keymaps = {
+			runDefault = "<leader>RR",
+			runWithArgs = "<leader>Rr",
+		},
+	},
+}
