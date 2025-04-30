2b137b7f4296212931fcb9030f984646d7fb84e9
thederpykrafter <thederpykrafter@gmail.com>
2025-04-29 21:29:51 -0400

    json formatter

---
+++ lua/thederpykrafter/plugins/json.lua
@@ -0,0 +1,14 @@
+return {
+	"VPavliashvili/json-nvim",
+	ft = "json", -- only load for json filetype
+	config = function()
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = { "*.json" },
+			callback = function() vim.cmd "JsonFormatFile" end,
+			group = vim.api.nvim_create_augroup(
+				"json_format",
+				{ clear = true }
+			),
+		})
+	end,
+}
