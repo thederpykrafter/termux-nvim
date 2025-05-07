420afd13b7fe76fefd3532eff4231a4a8c5c0b96
thederpykrafter <thederpykrafter@gmail.com>
2025-05-06 20:22:50 -0400

    colorful indentline

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,11 +1,33 @@
 return {
-	{ -- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help ibl`
-		main = "ibl",
-		opts = {
-			exclude = { filetypes = { "dashboard" } },
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {
+		exclude = { filetypes = { "dashboard" } },
+		indent = {
+			highlight = {
+				"RainbowRed",
+				"RainbowYellow",
+				"RainbowBlue",
+				"RainbowOrange",
+				"RainbowGreen",
+				"RainbowViolet",
+				"RainbowCyan",
+			},
 		},
 	},
+	config = function(_, opts)
+		require("ibl").setup(opts)
+		local hooks = require "ibl.hooks"
+		-- create the highlight groups in the highlight setup hook, so they are reset
+		-- every time the colorscheme changes
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+		end)
+	end,
 }
