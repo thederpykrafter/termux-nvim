788b7ba2fa8f27d02b05eba3db87a6962b066c25
thederpykrafter <thederpykrafter@gmail.com>
2025-05-21 20:40:26 -0400

    update lua-line

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -1,13 +1,3 @@
-local colors = {
-	gray = "#000000",
-	lightred = "#D16969",
-	blue = "#569CD6",
-	pink = "#C586C0",
-	black = "#262626",
-	white = "#D4D4D4",
-	green = "#608B4E",
-}
-
 return {
 	{
 		"nvim-lualine/lualine.nvim",
@@ -15,51 +5,7 @@ return {
 		opts = {
 			options = {
 				icons_enabled = true,
-				theme = {
-					normal = {
-						b = { fg = colors.green, bg = colors.black },
-						a = {
-							fg = colors.black,
-							bg = colors.green,
-							gui = "bold",
-						},
-						c = { fg = colors.white, bg = colors.black },
-					},
-					visual = {
-						b = { fg = colors.pink, bg = colors.black },
-						a = {
-							fg = colors.black,
-							bg = colors.pink,
-							gui = "bold",
-						},
-					},
-					inactive = {
-						b = { fg = colors.black, bg = colors.blue },
-						a = {
-							fg = colors.white,
-							bg = colors.gray,
-							gui = "bold",
-						},
-					},
-					replace = {
-						b = { fg = colors.lightred, bg = colors.black },
-						a = {
-							fg = colors.black,
-							bg = colors.lightred,
-							gui = "bold",
-						},
-						c = { fg = colors.white, bg = colors.black },
-					},
-					insert = {
-						b = { fg = colors.blue, bg = colors.black },
-						a = {
-							fg = colors.black,
-							bg = colors.blue,
-							gui = "bold",
-						},
-						c = { fg = colors.white, bg = colors.black },
-					},
-				},
+				theme = "codedark",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
@@ -70,17 +16,17 @@ return {
 				always_divide_middle = true,
 				globalstatus = true,
 				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
+					statusline = 100,
+					tabline = 100,
+					winbar = 100,
 				},
 			},
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
-				lualine_x = { "fileformat", "filetype" },
-				lualine_y = { "" },
+				lualine_x = { "lsp_status" },
+				lualine_y = { "fileformat", "filetype" },
 				lualine_z = { "location" },
 			},
 			inactive_sections = {
