53d36aea7fed37ceff2d626344c66daa057a521b
thederpykrafter <thederpykrafter@gmail.com>
2025-05-23 02:35:05 -0400

    disable lazy changes notify

--- init.lua
+++ init.lua
@@ -8,7 +8,14 @@ require('lazy').setup({
   },
   install = { colorscheme = { 'habamax' } },
   checker = { enabled = true },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
 })
+
+local checker = require('lazy.manage.checker')
+checker.report = function(notify) end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
